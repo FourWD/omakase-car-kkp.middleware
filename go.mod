fc5ac196a3d968b4467af4122685d3987ba7b2e4
puarns <puarns@gmail.com>
2023-08-17 10:27:55 +0700

    init

---
+++ go.mod
@@ -0,0 +1,11 @@
+module omakase-car-kkp.middleware
+
+go 1.21.0
+
+require github.com/FourWD/middleware v1.0.27
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	gorm.io/gorm v1.23.8 // indirect
+)
---
+++ orm/user.go
@@ -0,0 +1,18 @@
+package orm
+
+import "github.com/FourWD/middleware/orm"
+
+type User struct {
+	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
+	orm.GormModel
+
+	UserTypeID string `json:"user_type_id" query:"user_type_id" gorm:"type:varchar(36);"`
+
+	Username     string `json:"username" query:"username" gorm:"type:varchar(20);"`
+	Password     string `json:"password" query:"password" gorm:"type:varchar(20);"`
+	Firstname    string `json:"firstname" query:"firstname" gorm:"type:varchar(100);"`
+	Lastname     string `json:"lastname" query:"lastname" orm:"type:varchar(100);"`
+	FileAvatarID string `json:"file_avartar_id" query:"file_avartar_id" gorm:"type:varchar(36)"`
+	Mobile       string `json:"mobile" query:"mobile" gorm:"type:varchar(20);"`
+	Email        string `json:"email" query:"email" gorm:"type:varchar(20);"`
+}
