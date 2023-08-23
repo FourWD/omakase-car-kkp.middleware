c40b86cfd712b0716ae7ad0a8acdcebaa5e6890e
winra159@gmail.com <winra159@gmail.com>
2023-08-23 17:19:42 +0700

    add table

---
+++ orm/auction.go
@@ -0,0 +1,18 @@
+package orm
+
+import (
+	"time"
+
+	"github.com/FourWD/middleware/orm"
+)
+
+type auction struct {
+	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
+	orm.GormModel
+
+	ApproveDate time.Time `json:"approve_date" query:"approve_date" gorm:"type:time;"`
+	ApproveBy   string    `json:"approve_by" query:"approve_by" gorm:"type:varchar(50);"`
+	VehicleNo   string    `json:"vehicle_no" query:"vehicle_no" gorm:"type:varchar(20);"`
+	AuctionNo   string    `json:"auction_no" query:"auction_no" gorm:"type:varchar(20);"`
+	IsApprove   bool      `json:"is_approve" query:"is_approve" gorm:"type:bool;"`
+}
---
+++ orm/sync.go
@@ -0,0 +1,17 @@
+package orm
+
+import (
+	"time"
+
+	"github.com/FourWD/middleware/orm"
+)
+
+type sync struct {
+	ID string `json:"id" query:"id" gorm:"type:varchar(36);primary_key;"`
+	orm.GormModel
+
+	SyncDate time.Time `json:"sync_date" query:"sync_date" gorm:"type:time;"`
+	SyncBy   string    `json:"sync_by" query:"sync_by" gorm:"type:varchar(50);"`
+	SyncName string    `json:"sync_name" query:"sync_name" gorm:"type:varchar(50);"`
+	SyncType string    `json:"sync_type" query:"sync_type" gorm:"type:varchar(5);"`
+}
