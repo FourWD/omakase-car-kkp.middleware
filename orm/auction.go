214367cd1a5bfa4ae2ae241aca847e3ad99584e1
winra159@gmail.com <winra159@gmail.com>
2023-08-28 11:29:04 +0700

    new ver

--- orm/auction.go
+++ orm/auction.go
@@ -14,5 +14,6 @@ type Auction struct {
 	ApproveBy   string    `json:"approve_by" query:"approve_by" gorm:"type:varchar(50);"`
 	VehicleNo   string    `json:"vehicle_no" query:"vehicle_no" gorm:"type:varchar(20);"`
 	AuctionNo   string    `json:"auction_no" query:"auction_no" gorm:"type:varchar(20);"`
+	AuctionDate string    `json:"auction_date" query:"auction_date" gorm:"type:varchar(10);"`
 	IsApprove   bool      `json:"is_approve" query:"is_approve" gorm:"type:bool;"`
 }
